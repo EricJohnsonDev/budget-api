2f42a154ba1c6f3c93596b76a049fab4bac28461
FenderShredder512 <ejmetall@gmail.com>
2025-06-23 13:55:19 -0500

    first working query

--- main.go
+++ main.go
@@ -12,9 +12,54 @@ import (
 
 var db *sql.DB
 
+type ExpenseTx struct {
+	ID          int
+	Date        string
+	Amount      string
+	Institution string
+	Category    string
+	Subcategory string
+	Comment     string
+}
+
 func main() {
 	loadEnv()
+	db = connectToDb()
 
+	expenses, err := expensesByDate("2025-05-29")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Expenses found: %v\n", expenses)
+}
+
+// Query for expenses from a single day
+func expensesByDate(date string) ([]ExpenseTx, error) {
+	var expenses []ExpenseTx
+
+	rows, err := db.Query("SELECT * FROM tx_expenses WHERE \"Date\" = $1", date)
+	if err != nil {
+		return nil, fmt.Errorf("expensesByDate %q: %v", date, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var expense ExpenseTx
+		if err := rows.Scan(&expense.ID, &expense.Date, &expense.Amount, &expense.Institution, &expense.Category, &expense.Subcategory, &expense.Comment); err != nil {
+			return nil, fmt.Errorf("expensesByDate %q: %v", date, err)
+		}
+
+		expenses = append(expenses, expense)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("expensesByDate %q: %v", date, err)
+	}
+
+	return expenses, nil
+}
+
+func connectToDb() *sql.DB {
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbPwd := os.Getenv("DB_PWD")
@@ -41,7 +86,8 @@ func main() {
 		log.Fatal(pingErr)
 	}
 
-	fmt.Println("Connected!")
+	fmt.Println("Confirmed connection to db")
+	return db
 }
 
 func loadEnv() {
