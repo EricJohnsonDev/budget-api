92fc12fe38007c020c27c8ca80cc6551c148efa3
FenderShredder512 <ejmetall@gmail.com>
2025-07-14 15:34:02 -0500

    some date validation

--- handlers/config.go
+++ handlers/config.go
@@ -2,10 +2,20 @@ package handlers
 
 import (
 	"elelequent/prototypes/budget-api/dao/interfaces"
+	"net/http"
+	"regexp"
 )
 
 var dao interfaces.BudgetDao
 
 func SetDao(daoIn interfaces.BudgetDao) {
 	dao = daoIn
+}
+
+func setCommonHeaders(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func isValidDateFormat(dateIn string) bool {
+	return regexp.MustCompile(`\d{2}-\d{2}-\d{4}`).MatchString(dateIn)
 }
--- handlers/expenses.go
+++ handlers/expenses.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -11,19 +12,31 @@ func ExpensesByDates(w http.ResponseWriter, r *http.Request) {
 	startDate := r.Form.Get("start")
 	endDate := r.Form.Get("end")
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if !isValidDateFormat(startDate) {
+		http.Error(w, "Start date is required, but is either missing or not a valid format (expected MM-dd-YYYY)", http.StatusBadRequest)
+		return
+	}
+
+	if !isValidDateFormat(endDate) {
+		http.Error(w, "End date is missing or not a valid format (expected MM-dd-YYYY).  Restricting request to the start date.", http.StatusContinue)
+		endDate = startDate
+	}
 
 	expenses, err := dao.ExpensesByDate(startDate, endDate)
 
 	if err != nil {
+		log.Printf("ERROR Unable to retrieve expenses with start %s and end %s: %v", startDate, endDate, err)
 		http.Error(w, "An error occurred while retrieving data", http.StatusInternalServerError)
 		return
 	}
 
 	expensesJson, err := json.Marshal(expenses)
 	if err != nil {
+		log.Printf("ERROR Unable to encode JSON: %v", err)
 		http.Error(w, "An error occurred while processing data", http.StatusInternalServerError)
 	}
+
+	setCommonHeaders(w)
 
 	fmt.Fprintf(w, "{\"Expenses\": %v}", string(expensesJson))
 }
