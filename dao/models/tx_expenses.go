b3b97c8269559add2a1b7940dc893cacfa74d6b5
FenderShredder512 <ejmetall@gmail.com>
2025-08-01 16:12:07 -0500

    Updated Subcategory type from NullString to pointer b/c it simply seems easier rn

--- dao/models/tx_expenses.go
+++ dao/models/tx_expenses.go
@@ -1,13 +1,14 @@
 package models
 
-import "database/sql"
-
 type Tx_expenses struct {
 	ID          int
 	Date        string
 	Amount      string
 	Institution string
 	Category    string
-	Subcategory sql.NullString
+	// *string in place of sql.NullString
+	// See https://stackoverflow.com/questions/40092155/difference-between-string-and-sql-nullstring
+	// *string seems much easier to work with at this point
+	Subcategory *string
 	Comment     string
 }
